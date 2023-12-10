4690b4b87aab7029187d6b59b37753d100ce3327
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-12-10 20:25:21 -0300

    Update question1.c

--- Unidade_1/dynamic_allocation/question1.c
+++ Unidade_1/dynamic_allocation/question1.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(){
+  int interviewed, female_liked, male_liked;
+
+
+  printf("Enter the number of interviewed: ");
+  scanf("%d",&interviewed);
+
+}
