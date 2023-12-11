c17cb7fa766bd0f8ae04cb6e777abeb9d456cfa4
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-12-10 21:27:59 -0300

    Update question1.c

--- Unidade_1/dynamic_allocation/question1.c
+++ Unidade_1/dynamic_allocation/question1.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(){
+  //declaração das variaveis
   int interviewed, female_liked, male_liked;
 
 
   printf("Enter the number of interviewed: ");
   scanf("%d",&interviewed);
 
+  //alocação de memoria de um vetor e uma matriz
+  char *sex =(char *)malloc(interviewed*sizeof(char));
+  char *opinion =(char *)malloc(interviewed*sizeof(char));
+
+  //verificação da memoria disponivel
+  if (opinion==NULL || sex==NULL){
+    printf("Erro");
+    exit(1);
+  }
+
+  //preencher vetor e matriz
+  for (i=0; i<interwed;i++){
+    printf("\n Inform your gender F-female or M-male: " tolower(sex)); //verificar se foi digitado maiusculo ou minusculo e nao da erro
+    scanf("%c", &sex[i]);
+
+    printf("\n Let us know your opinion! \nDid you L-like or D-dislike the product?"); //verificar da mesma forma
+    scanf("%c",&opinion[i]);
+  }
+
+  
+//terminar
+
+  
 }
