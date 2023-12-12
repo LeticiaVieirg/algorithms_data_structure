0fe0ab0d4d580ffa6143485fbf1a555b8e91b358
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-12-12 19:36:21 -0300

    Update celsius_fahrenheit.c

--- Unidade_1/extra/book_exercise/celsius_fahrenheit.c
+++ Unidade_1/extra/book_exercise/celsius_fahrenheit.c
@@ -5,13 +5,16 @@
 #include <stdio.h>
 
 int main() {
+  //espaço para armazenar temperatura em celsius e fahrenheit
   float celsius,fahrenheit;
 
+  //capturar valor informado pelo usuario
   printf("enter a value of celsius:");
   scanf("%f",&celsius);
 
+  //realizar conversao
   fahrenheit=(1.8*celsius)+32;
-  printf("the value of fahrenheit is: " "%f.2", fahrenheit);
+  printf(" Temperatura em Fahrenheit: %f \n",fahrenheit);
 
 return 0;
 }
