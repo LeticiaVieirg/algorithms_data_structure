29df35a0b990f22dabf39ffd253c62fdf858aa96
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-12-16 15:19:17 -0300

    Update question2.c

--- Unidade_1/pointers/question2.c
+++ Unidade_1/pointers/question2.c
@@ -1,9 +1,10 @@
-
 #include <stdio.h>
 
 int main(void) {
   int x, *p;
   x = 100;
   p = x;
-  printf(“Valor de p = %p\tValor de *p = %d”, p, *p);
+  printf(“Valor de p = %p\t Valor de *p = %d”, p, *p);
 }
+//para que o codigo acima fique correto, basta colocar o & ao lado do x
+//p=&x
