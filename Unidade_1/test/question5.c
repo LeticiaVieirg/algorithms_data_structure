24871a6420e702f9c72a8314c996531fbc2cbeb0
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2023-12-17 16:15:48 -0300

    Update question5.c

--- Unidade_1/test/question5.c
+++ Unidade_1/test/question5.c
@@ -1,8 +1,7 @@
 #include <stdio .h >
 
 int main ( ) {
-	int i ; //o indice inicia no 1, e vai ate quando for menor ou igual a 100, e no proximo laço ele é multiplicado por 100
-
+	int i ;
 	for ( i =1 ; i <= 100 ; i *=100) {
 		if ( i==30 ) {
 			break ;
@@ -11,9 +10,9 @@ int main ( ) {
 			printf ("%2d\n" ,2* i ) ;
 		}
 		
-  }
+  	}
 	
-  printf ("Fim do laco !\n") ;
+printf ("Fim do laco !\n") ;
 	return 0;
 }
 
