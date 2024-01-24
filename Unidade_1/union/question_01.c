8a0121a41d31113f8b4f935fadfdd441a56e8bea
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-01-24 14:22:59 -0300

    Update question_01.c

--- Unidade_1/union/question_01.c
+++ Unidade_1/union/question_01.c
@@ -1,19 +1,15 @@
-/*Escreva um programa que implementa uma struct Pessoa com os campos nome, idade e gênero. O
-gênero deve ser armazenado como um enum com os valores MASCULINO e FEMININO. O programa
-deve ler os dados de uma pessoa e imprimir os dados da pessoa.*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef union genero{
-    char MASC_FEMI[10];
+    char MASCULINO_FEMININO[10];
 } Genero;
 
 typedef struct pessoa{
     char nome[20];
     int idade;
-    Genero gener;
+    Genero genero_pessoa;
 } Pessoa;
 
 void cadastro_pessoa(Pessoa *p){
@@ -21,25 +17,27 @@ void cadastro_pessoa(Pessoa *p){
     printf("=== Cadastro de Pessoas ===\n");
     printf("Informe o nome: ");
     scanf("%[^\n]", p->nome);
+    
     printf("Informe a idade: ");
     scanf("%d", &p->idade);
+    
     printf("Informe 1 para MASCULINO e 0 para FEMININO: ");
     scanf("%d", &opcao);
     if(opcao){
-        strcpy(p->gener.MASC_FEMI, "MASCULINO");
+        strcpy(p->genero_pessoa.MASCULINO_FEMININO, "MASCULINO");
     } else {
-        strcpy(p->gener.MASC_FEMI, "FEMININO");
+        strcpy(p->genero_pessoa.MASCULINO_FEMININO, "FEMININO");
     }
 }
 
 int main(void){
-    Pessoa *pessoa = malloc(sizeof(Pessoa));
-    cadastro_pessoa(pessoa);
+    Pessoa *p = malloc(sizeof(Pessoa));
+    cadastro_pessoa(p);
 
-    printf("Dados cadastrados: \n");
-    printf("Nome: %s\n", pessoa->nome);
-    printf("Idade: %d\n", pessoa->idade);
-    printf("Genero: %s\n", pessoa->gener.MASC_FEMI);
+    printf("===Dados cadastrados=== \n");
+    printf("Nome: %s\n", p->nome);
+    printf("Idade: %d\n", p->idade);
+    printf("Genero: %s\n", p->genero_pessoa.MASCULINO_FEMININO);
 
     return 0;
 }
