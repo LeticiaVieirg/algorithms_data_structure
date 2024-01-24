070c8abb6e14ce610e5889f58468723e3434349c
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-01-24 14:31:32 -0300

    Update question_02.c

--- Unidade_1/union/question_02.c
+++ Unidade_1/union/question_02.c
@@ -1,7 +1,3 @@
-/*Escreva um programa que implementa uma struct Produto com os campos nome, preço e tipo. O
-tipo deve ser armazenado como uma union que armazena um dos tipos: ALIMENTO, BEBIDA ou
-ELETRONICO. O programa deve ler os dados de um produto e imprimir os dados do produto.*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,10 +17,13 @@ void cadastro_pessoa(Produto *p){
     printf("=== Cadastro de Produto ===\n");
     printf("Informe o nome: ");
     scanf("%[^\n]", p->nome);
+    
     printf("Informe o preço: ");
     scanf("%f", &p->preco);
+    
     printf("Informe o tipo 1 Alimento, 2 Bebida, 3 Eletronico: ");
     scanf("%d", &opcao);
+    
     switch (opcao){
     case 1:
         strcpy(p->tipe.Tipo, "ALIMENTO");
@@ -42,7 +41,7 @@ int main(void){
     Produto *produto = malloc(sizeof(Produto));
     cadastro_pessoa(produto);
 
-    printf("Dados cadastrados: \n");
+    printf("===Dados cadastrados===\n");
     printf("Nome: %s\n", produto->nome);
     printf("Idade: %f\n", produto->preco);
     printf("Genero: %s\n", produto->tipe.Tipo);
