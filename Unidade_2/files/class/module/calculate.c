baf76c145e537c9129eb6d833d178e585dbf134f
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-02-05 20:53:16 -0300

    Update calculate.c

--- Unidade_2/files/class/module/calculate.c
+++ Unidade_2/files/class/module/calculate.c
@@ -1,1 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+#include "calculater.h"
 
+float adicao(float numero1, float numero2){
+    return numero1 + numero2;
+}
+
+float subtracao(float numero1, float numero2){
+    return numero1 - numero2;
+}
+
+float multiplicacao(float numero1, float numero2){
+    return numero1 * numero2;
+}
+
+float divisao(float numero1, float numero2){
+    if (numero2 == 0){
+        printf("Tipo de divisao desconsiderada!\n");
+        return 0;
+    }
+    else {
+        return numero1 / numero2;
+    }
+}
+
+float potenciacao(float base, float expoente){
+    return pow(base, expoente);
+}
