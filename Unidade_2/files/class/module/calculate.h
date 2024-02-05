59be21019580dc4b88830bc4be5386b3c44bf5c4
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-02-05 20:48:07 -0300

    Update calculate.h

--- Unidade_2/files/class/module/calculate.h
+++ Unidade_2/files/class/module/calculate.h
@@ -1,1 +1,16 @@
+/* Módulo para as operações aritimeticas básicas*/
 
+/* Função que retorna a soma de dois números e recebe como argumento dois números do tipo real*/
+float adicao(float numero1, float numero2);
+
+/* Função que retorna a subtração de dois números e recebe como argumento dois números do tipo real*/
+float subtracao(float numero1, float numero2);
+
+/* Função que retorna a multiplicação de dois números e recebe como argumento dois números do tipo real*/
+float multiplicacao(float numero1, float numero2);
+
+/* Função que retorna a divisao de dois números e recebe como argumento dois números do tipo real*/
+float divisao(float numero1, float numero2);
+
+/* Função que retorna a potenciação de dois números e recebe como argumento dois números do tipo real*/
+float potenciacao(float base, float expoente);
