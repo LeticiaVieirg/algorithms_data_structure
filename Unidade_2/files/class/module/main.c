6838447b590059f3738638d3ae7b2b3c737badea
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-02-05 20:56:06 -0300

    Update main.c

--- Unidade_2/files/class/module/main.c
+++ Unidade_2/files/class/module/main.c
@@ -15,32 +15,32 @@ void operacao(){
 int escolha;
 
 while (1) {
-        printf("\nEscolha uma opcao:\n");
-        printf("1. Adicao\n");
-        printf("2. Subtracao\n");
-        printf("3. Multiplicacao\n");
-        printf("4. Divisao\n");
-        printf("5. Potenciacao\n");
-        printf("6. Sair\n");
-        scanf("%d", &escolha);
-
-        if (escolha == 1) {
-            printf("Resultado: %.2f\n", adicao(primeiro_numero, segundo_numero));
-        } else if (escolha == 2) {
-            printf("Resultado: %.2f\n", subtracao(primeiro_numero, segundo_numero));
-        } else if (escolha == 3) {
-            printf("Resultado: %.2f\n", multiplicacao(primeiro_numero, segundo_numero));
-        } else if (escolha == 4) {
-            printf("Resultado: %.2f\n", divisao(primeiro_numero, segundo_numero));
-        } else if (escolha == 5) {
-            printf("Resultado: %.2f\n", potenciacao(primeiro_numero, segundo_numero));
-        } else if (escolha == 6) {
-            printf("Saindo...\n");
+    printf("\nEscolha uma opcao:\n");
+    printf("1. Adicao\n");
+    printf("2. Subtracao\n");
+    printf("3. Multiplicacao\n");
+    printf("4. Divisao\n");
+    printf("5. Potenciacao\n");
+    printf("6. Sair\n");
+    scanf("%d", &escolha);
+        
+    if (escolha == 1) {
+        printf("Resultado: %.2f\n", adicao(numero1, numero2));
+    } else if (escolha == 2) {
+        printf("Resultado: %.2f\n", subtracao(numero1, numero2));
+    } else if (escolha == 3) {
+        printf("Resultado: %.2f\n", multiplicacao(numero1, numero2));
+    } else if (escolha == 4) {
+        printf("Resultado: %.2f\n", divisao(numero1, numero2));
+    } else if (escolha == 5) {
+        printf("Resultado: %.2f\n", potenciacao(numero1, numero2));
+    } else if (escolha == 6) {
+        printf("Saindo...\n");
             break; // Sai do loop
-        } else {
-            printf("Opcao invalida!\n");
-        }
+    } else {
+        printf("Opcao invalida!\n");
     }
+}
 
 int main(){
   coleta_numeros();
