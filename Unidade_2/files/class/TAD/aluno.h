a1369da6a0d3a6767a581310b7220260ac6a17f9
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-02-05 21:07:06 -0300

    Update aluno.h

--- Unidade_2/files/class/TAD/aluno.h
+++ Unidade_2/files/class/TAD/aluno.h
@@ -1,1 +1,11 @@
+/*Renomeacao de um novo tipo: Aluno*/
+typedef struct aluno Aluno;
 
+/*Função que aloca memória para um struct aluno, os dados sao recebidos via teclado e retorna um ponteiro*/
+Aluno *recebe_dados(void);
+
+/*Função que imprime os dados de um aluno, recebe um ponteiro de Aluno como parâmetro*/
+void imprime_dados(Aluno * estudante);
+
+/*Função que libera a memória, como parametro recebe um ponteiro de Aluno*/
+void libera(Aluno * estudante);
