f22fcb648989350766c31d2bf0f86e31e6173417
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-02-11 21:01:33 -0300

    Update ingresso.h

--- Unidade_2/TAD/ingresso.h
+++ Unidade_2/TAD/ingresso.h
@@ -3,16 +3,16 @@ typedef struct ingresso Ingresso;//mudando o nome da struct ingresso so para Ing
 
 /*Funcao que armazena os dados inseridos pelo usuario, 
   Recebe como parametros um ponteiro de ingresso*/
-void preenche(Ingresso *i);
+void preenche(Ingresso *ingressos);
 
 /*Funcao que exibe na tela os dados armazenados, 
   Recebe como parametro um ponteiro de ingresso*/
-void imprime(Ingresso *i);
+void imprime(Ingresso *ingressos);
 
 /*Funcao que altera o preco do ingresso escolhido pelo usuario,
   Recebe como parametros um ponteiro de ingresso e um valor real*/
-void altera_preco(Ingresso *i, float valor);
+void altera_preco(Ingresso *ingressos, float valor);
 
 /*Funcao que exibe o menor e o maior valores dos ingressos armazenados, 
   Recebe como parametros um inteiro e um ponteiro de ingresso*/
-void imprime_menor_maior_preco(int n, Ingresso *vet);
+void maior_e_menor_valor(int quantidade, Ingresso *id);
