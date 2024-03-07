432b64aeb1c945c662bf4d1c7e666488bf46ffad
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-07 20:08:39 -0300

    Create calculadora.h

---
+++ Unidade_2/TAD/tad_calculadora/calculadora.h
@@ -0,0 +1,17 @@
+/* Função para adição, recebe dois numeros reais*/
+float adicao(float, float);
+
+/* Função para subtração, recebe dois numeros reais*/
+float subtracao(float, float);
+
+/* Função para multiplicação, recebe como parametro dois numeros reais*/
+float multiplicacao(float, float);
+
+/* Função para divisão, recebe dois nummeros reais*/
+float divisao(float, float);
+
+/*Função para potenciação, recebe dois numeros reais*/
+float potenciacao(float, float);
+
+/*Função para exibir o menu*/
+void exibirMenu();
