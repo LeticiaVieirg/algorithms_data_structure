5fcb06ecc59811c6fc32308f35a41d132b7668e3
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-18 14:35:43 -0300

    Update pilha.h

--- Unidade_3/stack/list01/question01/pilha.h
+++ Unidade_3/stack/list01/question01/pilha.h
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct pilha Pilha;
+
+Pilha *pilha_cria(void);
+void pilha_push(Pilha *p, float v);
+float pilha_pop(Pilha *p);
+int pilha_vazia(Pilha *p);
+void pilha_libera(Pilha *p);
