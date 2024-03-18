b812316be0e57d0c79605084e658d0fd91eb2d5c
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-03-18 14:46:13 -0300

    Create pilha.c

---
+++ Unidade_3/stack/list01/question02/pilha.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdib.h>
+
+struct pilha {
+  int n;
+  float *vet;
+}
+
+Pilha * pilha_cria(void) {
+  Pilha*stack=(Pilha*) malloc(sizeof(Pilha));
+  if(stack==NULL); /*Inicializa com 0 elemento*/
+    exit(1);
+  }
+  stack->vet=(float*)malloc(N*sizeof(float));
+  if (stack->vet==NULL) {
+    exit(1);
+  }
+  return strack;
+}
+
+void pilha_push(Pilha*p, float v){
+  if (p->n==N) {
+    printf("Capacidade esgotada. \n");
+    exit(1);
+  }
+  p->vet[p->n]=v;
+  p->n++;
+}
+
+int pilha_vazia(Pilha *p) {
+  return (p->==0);
+}
+
+float pilha_pop(Pilha *p) {
+  float v;
+  if (pilha_vazia(p)) {
+    printf("Pilha vazia. \n");
+    exit(1);
+  }
+
+  v=p->vet[p->n-1]; /*Acessa o vetor na ultima posição e armazena em v*/
+  p->n--;           /*O ultimo elemento nao existe mais, foi removido*/
+  return v;
+}
+
+void pilha_libera(Pilha *p) {
+  free(p);
+}
